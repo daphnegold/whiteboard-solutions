4966711ad03290a42ba50456b2aae40715162d19
Daphne <dg1181@nyu.edu>
2016-01-31 21:15:35 -0800

    Fixed issue with clock angle problem

--- clock-angle.rb
+++ clock-angle.rb
@@ -1,12 +1,15 @@
 # Write a function that takes in a time (hour and minute) and outputs the angle between the hour and minute hands on a clock.
 
-def angle(hour, minute)
+def angle(hour, minutes)
   if hour == 12
     hour = 0
   end
-
-  hour_angle = hour * (360/12)
-  minute_angle = minute * (360/60)
+  # hour hand math: degrees per hour, degrees per minute
+    # 360 / 12 = 30, 360 / (12 * 60) = 0.5
+  # minute hand math: degrees per minute
+    # 360 / 60 = 6
+  hour_angle = hour * 30 + minutes * 0.5
+  minute_angle = minutes * 6
 
   return (hour_angle - minute_angle).abs
 end
